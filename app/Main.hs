30b65c3136aa74a39d5bcc3f8762d0e40060315a
Kevin <khine@ucsd.edu>
2018-07-24 16:05:37 -0700

    Took necessary files from synthesizer

--- app/Main.hs
+++ app/Main.hs
@@ -11,21 +11,9 @@ import qualified Data.Attoparsec.ByteString as A
 import qualified Data.AttoLisp as L
 import qualified Data.ByteString as ByteString
 
--- | Parameters for constraint solving
-defaultHornSolverParams = HornSolverParams {
-  pruneQuals = True,
-  isLeastFixpoint = False,
-  optimalValuationsStrategy = MarcoValuations,
-  semanticPrune = True,
-  agressivePrune = True,
-  candidatePickStrategy = InitializedWeakCandidate,
-  constraintPickStrategy = SmallSpaceConstraint,
-  solverLogLevel = 0
-  }
-
-data ProgramOptions = ProgramOptions { 
+data ProgramOptions = ProgramOptions {
   printResult :: Bool,
-  outputFile :: String 
+  outputFile :: String
 } deriving (Show, Eq)
 
 version = putStrLn "musfix 0.1.0.0"
@@ -35,10 +23,10 @@ main :: IO ()
 main = do
   args <- getArgs
   parseArgs args
-        
+
 parseArgs :: [String] -> IO ()
 parseArgs as    = mapM_ parseArg as
-        
+
 parseArg :: String -> IO ()
 parseArg "-h"   = usage     >> exitSuccess
 parseArg "-v"   = version   >> exitSuccess
@@ -47,16 +35,16 @@ parseArg f      = readConstraints f
 readConstraints :: String -> IO ()
 readConstraints f = do
     s <- ByteString.readFile f
-    let lisp = topSExprs $ s in 
+    let lisp = topSExprs $ s in
       let ins = topInputs lisp in
-      putStrLn $ show ins 
-      
-                
+      putStrLn $ show ins
+
+
 topSExprs :: ByteString -> [L.Lisp]
 topSExprs l = case A.parseOnly (A.many1 L.lisp) l of
     Left err -> error $ "ill formatted lisp: " ++ err
     Right ins -> ins
-    
+
 topInputs :: [L.Lisp] -> [InputExpr]
 topInputs ls = map p ls
   where
