60aaebce8a17dec2b786c227438284616a09cacc
Conner Irwin <cjirwin@ucsd.edu>
2018-07-24 14:02:32 -0700

    Added file parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -22,18 +22,19 @@ defaultHornSolverParams = HornSolverParams {
   constraintPickStrategy = SmallSpaceConstraint,
   solverLogLevel = 0
   }
-  
+
+data ProgramOptions = ProgramOptions { 
+  printResult :: Bool,
+  outputFile :: String 
+} deriving (Show, Eq)
+
 version = putStrLn "musfix 0.1.0.0"
 usage   = putStrLn "Usage: musfix [file ...]"
 
 main :: IO ()
 main = do
-    args <- getArgs
-    parseArgs args
-    print $ is
-    where
-        ls = topSExprs "(qualif Neg ((v1 int) (v2 int)) (+ v1 23))\n(qualif Neg ((v int))   (<= v 0))"
-        is = topInputs ls
+  args <- getArgs
+  parseArgs args
         
 parseArgs :: [String] -> IO ()
 parseArgs as    = mapM_ parseArg as
@@ -42,6 +43,14 @@ parseArg :: String -> IO ()
 parseArg "-h"   = usage     >> exitSuccess
 parseArg "-v"   = version   >> exitSuccess
 parseArg f      = readConstraints f
+
+readConstraints :: String -> IO ()
+readConstraints f = do
+    s <- ByteString.readFile f
+    let lisp = topSExprs $ s in 
+      let ins = topInputs lisp in
+      putStrLn $ show ins 
+      
                 
 topSExprs :: ByteString -> [L.Lisp]
 topSExprs l = case A.parseOnly (A.many1 L.lisp) l of
@@ -50,8 +59,8 @@ topSExprs l = case A.parseOnly (A.many1 L.lisp) l of
     
 topInputs :: [L.Lisp] -> [InputExpr]
 topInputs ls = map p ls
-    where
-        p l = case L.parse parseInputExpr l of
-            L.Success i -> i
-            L.Error r -> error $ "bad input: " ++ r ++ "\n while attempting to parse lisp:" ++ (show l)
+  where
+    p l = case L.parse parseInputExpr l of
+        L.Success i -> i
+        L.Error r -> error $ "bad input: " ++ r ++ "\n while attempting to parse lisp:" ++ (show l)
     
--- src/Language/SMT/Parser.hs
+++ src/Language/SMT/Parser.hs
@@ -12,20 +12,32 @@ import qualified Data.Map as Map
 import Data.Map (Map, (!))
 import Data.Scientific as S
 
--- | Invert a map (must be 1-to-1)
-invertTokenMap :: Ord a => Map a String -> Map T.Text a
-invertTokenMap m = Map.fromList m'
-    where
-        xs = Map.toList m
-        m' = map f xs
-        f (k, v) = ((T.pack v), k)
-
-{- Operators -}
--- TODO: This does not work, some operators are overloaded!
-unaryOps = invertTokenMap unOpTokens
-binaryOps = invertTokenMap binOpTokens
-
-{- Sorts -}
+-- | Unary operators
+unaryOps :: Map T.Text UnOp 
+unaryOps = Map.fromList [ ("not",     Not)
+                        , ("-",       Neg)
+                        ]
+                       
+-- | Binary operators 
+binaryOps :: Map T.Text BinOp 
+binaryOps = Map.fromList [ ("*",    Times)
+                         , ("+",     Plus)
+                         , ("-",    Minus)
+                         , ("==",      Eq)
+                         , ("=",       Eq) -- TODO: Do we want to support this?
+                         , ("!=",     Neq)
+                         , ("<",       Lt)
+                         , ("<=",      Le)
+                         , (">",       Gt)
+                         , (">=",      Ge)
+                         , ("&&",     And)
+                         , ("||",      Or)
+                         , ("==>",Implies)
+                         , ("=>", Implies) -- TODO: Same as above
+                         , ("<==>",   Iff)
+                         ]
+                         
+-- | Variable sorts
 sorts :: Map T.Text Sort 
 sorts = Map.fromList [ ("int",   IntS),
                        ("bool", BoolS)
@@ -33,82 +45,85 @@ sorts = Map.fromList [ ("int",   IntS),
 
 {- Top-level Statements -}
 data InputExpr =
-     Qualifier T.Text [Formula] Formula          -- ^ Qualifier with name, variables and equation
-   | Constraint Formula                          -- ^ Constraint equation
-   | UnknownPredicate T.Text Formula             -- ^ Unknown logical predicate (to find)
-   deriving (Show, Eq, Ord)
+    Qualifier T.Text [Formula] Formula          -- ^ Qualifier with name, variables and equation
+  | WFConstraint T.Text Formula                 -- ^ Well-formed predicate constraint
+  | HornConstraint Formula                      -- ^ Horn constraint
+  deriving (Show, Eq, Ord)
    
 checkVars :: [Formula] -> Bool
 checkVars fs = foldr check True fs
-    where
-        check :: Formula -> Bool -> Bool
-        check f r = case f of
-            (Var s i) -> r
-            _ -> False
+  where
+    check :: Formula -> Bool -> Bool
+    check f r = case f of
+        (Var s i) -> r
+        _ -> False
 
 instance FromLisp InputExpr where
-    parseLisp (List [(Symbol "qualif"), (Symbol n), List xs, y]) = do
-            vars <- mapM parseFormula xs
-            if not $ checkVars vars then do
-                fail $ "invalid expressions in variable list"
-            else do
-                formula <- parseFormula y
-                return $ Qualifier n vars formula
-    parseLisp (List [(Symbol "constraint"), y]) = do
-            formula <- parseFormula y
-            return $ Constraint formula
-    parseLisp (List [(Symbol "wf"), (Symbol n), x]) = do
-            var <- parseFormula x
-            case var of
-                (Var s i) -> return $ UnknownPredicate n var
-                _ -> fail "predicate parameter is not a variable"
-    parseLisp _ = fail "unknown top-level construct"
+  -- qualifiers
+  parseLisp (List [(Symbol "qualif"), (Symbol n), List xs, y]) = do
+      vars <- mapM parseFormula xs
+      if not $ checkVars vars then do
+          fail $ "invalid expressions in variable list"
+      else do
+          formula <- parseFormula y
+          return $ Qualifier n vars formula
+  -- well-formed constraint
+  parseLisp (List [(Symbol "wf"), (Symbol n), x]) = do
+    var <- parseFormula x
+    case var of
+        (Var s i) -> return $ WFConstraint n var
+        _ -> fail "predicate parameter is not a variable"
+  -- horn constraint
+  parseLisp (List [(Symbol "constraint"), y]) = do
+      formula <- parseFormula y
+      return $ HornConstraint formula
+  parseLisp _ = fail "unknown top-level construct"
     
 parseInputExpr :: Lisp -> Parser InputExpr
 parseInputExpr = parseLisp 
 
 {- Formulas -}
 instance FromLisp Formula where
-    -- basic literals
-    parseLisp (Symbol "False")          = pure $ BoolLit False
-    parseLisp (Symbol "True")           = pure $ BoolLit True
-    parseLisp (Number n)                = case S.floatingOrInteger n of
-                                            Left f -> fail $ "non-integral value not supported " ++ (show n)
-                                            Right i -> pure $ IntLit i
-    parseLisp (Symbol v)                = pure $ Var AnyS (T.unpack v)
-    -- for all
-    parseLisp (List [(Symbol "forall"), x, y])  = do
+  -- basic literals
+  parseLisp (Symbol "False")          = pure $ BoolLit False
+  parseLisp (Symbol "True")           = pure $ BoolLit True
+  parseLisp (Number n)                = case S.floatingOrInteger n of
+      Left f -> fail $ "non-integral value not supported " ++ (show n)
+      Right i -> pure $ IntLit i
+  parseLisp (Symbol v)                = pure $ Var AnyS (T.unpack v)
+  -- for all
+  parseLisp (List [(Symbol "forall"), x, y])  = do
+      xExpr <- parseFormula x
+      yExpr <- parseFormula y
+      return $ All xExpr yExpr
+  -- unary operation
+  parseLisp (List [(Symbol f), a])
+      | Map.member f unaryOps     = do
+          let op = unaryOps ! f
+          expr <- parseFormula a
+          return $ Unary op expr
+  -- logical predicate application
+  parseLisp (List [(Symbol p), x])
+      | T.head p == '$'             = do
+          arg <- parseFormula x
+          return $ Pred AnyS (T.unpack p) []
+  -- variable sort assignment
+  parseLisp (List [(Symbol v), (Symbol s)])
+      | Map.member s sorts        = do
+          let sort = sorts ! s
+          return $ Var sort (T.unpack v)
+      | otherwise                 = fail $ "unrecognized sort " ++ (T.unpack s)
+  -- binary operation
+  parseLisp (List [(Symbol f), x, y]) = do
+      let maybeOp = Map.lookup f binaryOps
+      case maybeOp of
+        Nothing -> fail $ "invalid binary op " ++ (T.unpack f)
+        Just (op) -> do
             xExpr <- parseFormula x
             yExpr <- parseFormula y
-            return $ All xExpr yExpr
-    -- unary operation
-    parseLisp (List [(Symbol f), a])
-        | Map.member f unaryOps     = do
-            let op = unaryOps ! f
-            expr <- parseFormula a
-            return $ Unary op expr
-    -- logical predicate application
-    parseLisp (List [(Symbol p), x])
-        | T.head p == '$'             = do
-            arg <- parseFormula x
-            return $ Pred AnyS (T.unpack p) []
-    -- variable sort assignment
-    parseLisp (List [(Symbol v), (Symbol s)])
-        | Map.member s sorts        = do
-            let sort = sorts ! s
-            return $ Var sort (T.unpack v)
-        | otherwise                 = fail $ "unrecognized sort " ++ (T.unpack s)
-    -- binary operation
-    parseLisp (List [(Symbol f), x, y]) = do
-        let maybeOp = Map.lookup f binaryOps
-        case maybeOp of
-            Nothing -> fail $ "invalid binary op " ++ (T.unpack f)
-            Just (op) -> do
-                xExpr <- parseFormula x
-                yExpr <- parseFormula y
-                return $ Binary op xExpr yExpr
-        
-    parseLisp f = fail $ "cannot read formula: " ++ (show f)
+            return $ Binary op xExpr yExpr
+      
+  parseLisp f = fail $ "cannot read formula: " ++ (show f)
         
 parseFormula :: Lisp -> Parser Formula
 parseFormula l = parseLisp l
