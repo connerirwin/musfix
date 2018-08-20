b53d04b9363e7a18406e5982b711d099a5e81084
Kevin <khine@ucsd.edu>
2018-08-20 13:01:46 -0700

    Shell commands now only run after successfully building

--- profile.sh
+++ profile.sh
@@ -2,6 +2,6 @@
 # Runs musfix profiling
 
 PROFILE_FLAGS='--work-dir .stack-work-profile'
-stack ${PROFILE_FLAGS} --profile build
+stack ${PROFILE_FLAGS} --profile build &&
 stack ${PROFILE_FLAGS} exec -- musfix "$@"
 
--- verify.sh
+++ verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Compares the output against the rubric
 
-stack build
+stack build &&
 
 TEST_RUN='stack exec -- musfix --verbose test/*.smt2'
 diff <(${TEST_RUN}) correct_program_output
