b5b1b05d57e65a09651aac2979cedf48eb64b502
Kevin <khine@ucsd.edu>
2018-09-03 15:55:55 -0700

    Golden test cleanup

--- test/MusfixTests.hs
+++ test/MusfixTests.hs
@@ -13,20 +13,24 @@ import Test.Tasty.Golden
 main :: IO ()
 main = defaultMain =<< goldenTests
 
--- crash, cut, elim, minimize, neg, pos, proof, sample, todo]
--- TODO instead of writing to file, create internal string
+testDirs = ["crash", "cut", "elim", "minimize", "neg", "pos", "proof", "sample", "todo"]
+
 goldenTests :: IO TestTree
 goldenTests = do
-    smt2Files <- findByExtension [".smt2"] "test/."
-    return $ testGroup "Smt2 golden tests" [
-      goldenVsString (takeBaseName smt2File) (replaceExtension smt2File ".golden")
-        $ action smt2File
-        | smt2File <- smt2Files
-      ]
-  where
-    action :: FilePath -> IO LBS.ByteString
-    action inFile = do
-      readConstraints (ProgramOptions False False "test_output.tmp" True False) inFile
-      o <- LBS.readFile "test_output.tmp"
-      removeFile "test_output.tmp"
-      return o
+  testGroups <- mapM mkTestGroup testDirs
+  return $ testGroup "Smt2 Golden Tests" testGroups
+
+mkTestGroup :: FilePath -> IO TestTree
+mkTestGroup dir = do
+  smt2Files <- findByExtension [".smt2"] $ "test/" ++ dir
+  return $ testGroup dir [mkTest smt2File | smt2File <- smt2Files]
+
+mkTest :: FilePath -> TestTree
+mkTest f = goldenVsString (takeBaseName f) (replaceExtension f ".golden") $ action f
+
+action :: FilePath -> IO LBS.ByteString
+action inFile = do
+  readConstraints (ProgramOptions False False "test_output.tmp" True False) inFile
+  o <- LBS.readFile "test_output.tmp"
+  removeFile "test_output.tmp"
+  return o
